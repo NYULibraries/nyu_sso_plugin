b4d9575bb2f4028207279616b0a5547d90d15878
Kate Pechekhonova <ekate@nyu.edu>
2016-07-07 10:33:35 -0400

    Modified login url

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -4,4 +4,4 @@ require "uri"
 my_routes = [File.join(File.dirname(__FILE__), "routes.rb")]
 ArchivesSpace::Application.config.paths['config/routes'].concat(my_routes)
 
-AppConfig[:ssologin_url]="#{AppConfig[:backend_url]}/auth/doorkeeper"
+AppConfig[:ssologin_url]="#{AppConfig[:backend_url]}/auth/nyulibraries"
