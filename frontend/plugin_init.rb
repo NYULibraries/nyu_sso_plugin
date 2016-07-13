8c00a825836744ba6721d1f32e0a2749f5865d8d
Kate Pechekhonova <ekate@nyu.edu>
2016-07-13 13:44:09 -0400

    Branch for prod env

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -4,6 +4,6 @@ require "uri"
 my_routes = [File.join(File.dirname(__FILE__), "routes.rb")]
 ArchivesSpace::Application.config.paths['config/routes'].concat(my_routes)
 
-AppConfig[:ssologin_url]="http://aspace-stage.dlts.org/auth/nyulibraries"
+AppConfig[:ssologin_url]="http://aspace-prod.dlts.org/auth/nyulibraries"
 
 AppConfig[:ssologout_url]="https://login.library.nyu.edu/logged_out"
